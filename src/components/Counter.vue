10f988546cdd66985e115f0ca0037fbc92200ab1
Zhaoyu <weixiaobo88@gmail.com>
2019-08-05 15:48:30 +0800

    finish counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="counter">
+    <h1>Counter</h1>
+    <p>
+    </p>
+    <p>
+      <button @click="decrement">-</button>
+      <span>{{count}}</span>
+      <button @click="increment">+</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  data() {
+    return { count: 0 };
+  },
+  methods: {
+    increment() {
+      this.count += 1;  
+    },
+    decrement() {
+      this.count -= 1;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+button {
+  margin: 0 20px;
+}
+</style>
